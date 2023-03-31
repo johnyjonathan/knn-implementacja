8f5a2903f1c6ec133e525575b9b0d5da7ded1156
johnyjonathan <michaltaszycki@hotmail.com>
2023-03-31 20:37:39 +0200

    final app

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 from utilities import functions, preprocessing, charts
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.metrics import accuracy_score, classification_report
+import numpy
 # Setttings
 N_NEAREST_NEIGHBORS = 9
 
@@ -12,26 +13,31 @@ N_NEAREST_NEIGHBORS = 9
 file_path = 'data/car.data'
 data = functions.readData(file_path)
 
-# Split data, x - attributes, y - class
+# Split data, x - attributes, y - classes
 x, y = preprocessing.getXandY(data)
 
 # Train and test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
 # Set up model
-model = KNeighborsClassifier(n_neighbors=N_NEAREST_NEIGHBORS)
+model = KNeighborsClassifier(n_neighbors=N_NEAREST_NEIGHBORS, metric='jaccard')
 
 # Train model
 model.fit(x_train, y_train)
 
+# Get graph of model
+model_graph = model.kneighbors_graph(x_train, mode='distance', n_neighbors=N_NEAREST_NEIGHBORS)
+
 # Test model / accuracy
 accuracy = model.score(x_test, y_test)
-
 print('Accuracy: ', accuracy)
 
 # Predict
 predictions = model.predict(x_test)
-
+accuracy2 = accuracy_score(y_test, predictions)
+print('Accuracy2: ', accuracy2)
+report = classification_report(y_test, predictions)
+print('Report: ', report)
 # Get class and attrs names
 values_map = preprocessing.preprocessData(data)[2]
 attrs_values_map = preprocessing.preprocessData(data)[3]
@@ -44,5 +50,7 @@ for idx, val in enumerate(predictions):
 
 
 # Show graphs
+charts.showModelGraph(model_graph, x_train, y_train)
 charts.showClassesGraph(predictions, y_test, values_map)
 charts.showConfusionMatrixGraph(predictions, y_test)
+charts.showBarClassesGraph(data['class'])
--- utilities/charts.py
+++ utilities/charts.py
@@ -19,3 +19,28 @@ def showConfusionMatrixGraph(predicted, actual):
     plt.xlabel('Predicted label')
     plt.ylabel('True label')
     plt.show()
+
+def showBarClassesGraph(column_):
+    class_count = column_.value_counts()
+    plt.bar(class_count.index, class_count.values)
+    plt.xlabel('Klasy')
+    plt.ylabel('Liczba próbek')
+    plt.title('Ilość poszczególnych klas w zbiorze danych "Car Evaluation Data Set"')
+    plt.show()
+
+def showModelGraph(model_graph, X, y):
+    neighbors_array = model_graph.toarray()
+    X_np = np.array(X)
+    unique_labels = np.unique(y)
+    for label in unique_labels:
+        plt.scatter(X_np[y == label, 0], X_np[y == label, 1], label=f'Klasa {label}')
+
+    # Rysowanie linii łączących sąsiednie punkty
+    for i in range(neighbors_array.shape[0]):
+        for j in range(neighbors_array.shape[1]):
+            if neighbors_array[i, j] == 1:
+                plt.plot([X_np[i, 0], X_np[j, 0]], [X_np[i, 1], X_np[j, 1]], 'r--', alpha=0.5)
+
+    plt.title('Wykres sąsiedztwa dla k-NN')
+    plt.legend()
+    plt.show()
